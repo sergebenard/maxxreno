fbfc59cc4451aef4ec66bbe0bd3d0390215b2d1f
Serge <serge.benard@gmail.com>
2022-01-24 15:51:01 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.js",
+    "./src/**/*.htm",
+    "./src/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+let mix = require('laravel-mix');
+
+require('laravel-mix-svelte');
+
+mix.js("src/js/app.js", "public/js/")
+    .postCss("src/css/app.css", "public/css/", [
+        require("tailwindcss"),
+    ])
+    .copy("src/html/index.html", "public/index.html")
+    .svelte();
